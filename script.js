735100fa41a4ef2293b320d1bf476f5f3ac18e75
Hanalia <davethe@naver.com>
2022-09-05 20:21:43 +0900

    add google auth2

--- script.js
+++ script.js
@@ -58,3 +58,65 @@ searchBtn.addEventListener("click", () => {
     return;
   }
 });
+
+// //login function
+// /*
+//  * Create form to request access token from Google's OAuth 2.0 server.
+//  */
+// function oauthSignIn() {
+//   // Google's OAuth 2.0 endpoint for requesting an access token
+//   var oauth2Endpoint = "https://accounts.google.com/o/oauth2/v2/auth";
+
+//   // Create <form> element to submit parameters to OAuth 2.0 endpoint.
+//   var form = document.createElement("form");
+//   form.setAttribute("method", "GET"); // Send as a GET request.
+//   form.setAttribute("action", oauth2Endpoint);
+
+//   // Parameters to pass to OAuth 2.0 endpoint.
+//   var params = {
+//     client_id:
+//       "414139409173-j37fthobs44sq4vaofurc47u9pogdnh3.apps.googleusercontent.com",
+//     redirect_uri: "https://dabid-gh-pages.github.io",
+//     response_type: "token",
+//     scope: "https://www.googleapis.com/auth/drive.metadata.readonly",
+//     include_granted_scopes: "true",
+//     state: "pass-through value",
+//   };
+
+//   // Add form parameters as hidden input values.
+//   for (var p in params) {
+//     var input = document.createElement("input");
+//     input.setAttribute("type", "hidden");
+//     input.setAttribute("name", p);
+//     input.setAttribute("value", params[p]);
+//     form.appendChild(input);
+//   }
+
+//   // Add form to page and submit it to open the OAuth 2.0 endpoint.
+//   document.body.appendChild(form);
+//   form.submit();
+// }
+
+function handleCredentialResponse(response) {
+  console.log("Encoded JWT ID token: " + response.credential);
+  const responsePayload = decodeJwtResponse(response.credential);
+
+  console.log("ID: " + responsePayload.sub);
+  console.log("Full Name: " + responsePayload.name);
+  console.log("Given Name: " + responsePayload.given_name);
+  console.log("Family Name: " + responsePayload.family_name);
+  console.log("Image URL: " + responsePayload.picture);
+  console.log("Email: " + responsePayload.email);
+}
+window.onload = function () {
+  google.accounts.id.initialize({
+    client_id:
+      "414139409173-j37fthobs44sq4vaofurc47u9pogdnh3.apps.googleusercontent.com",
+    callback: handleCredentialResponse,
+  });
+  google.accounts.id.renderButton(
+    document.getElementById("buttonDiv"),
+    { theme: "outline", size: "large" } // customization attributes
+  );
+  google.accounts.id.prompt(); // also display the One Tap dialog
+};
